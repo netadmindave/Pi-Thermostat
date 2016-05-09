7281ff13d381867c3abb26f55296ffdef927bb75
netadmindave <netadmindave@gmail.com>
2016-05-08 22:24:06 -0400

    Update thermostat.py

--- thermostat.py
+++ thermostat.py
@@ -5,17 +5,17 @@ from datetime import timedelta
 import os
 import MySQLdb as mdb
 import sys
-import Adafruit_DHT
+import smbus
 import RPi.GPIO as GPIO
 
 #Global Variables
-Num_AC_Units = 4
+Num_AC_Units = 2
 mysql_user = 'thermostat'
 mysql_password = 'password'
 mysql_location = 'localhost'
 mysql_DB = 'thermostats'
 mysql_table = 'Thermostats'
-sensor = Adafruit_DHT.DHT22
+GPIO.setmode(GPIO.BCM)
 
 
 #Connect to Database 
@@ -36,10 +36,12 @@ for loop in range(0, Num_AC_Units):
 		GPIO.output(Compressor_Pin, 1)
 		Fan_Status = False
 		Compressor_Status = False
-		cursor.execute("UPDATE Thermostats Set  Fan_Status = %s, Compressor_Status = %s WHERE Id = %s", (Fan_Status, Compressor_Status, loop))
+		Compressor_Time = 10
+		Fan_Time = 10
+		cursor.execute("UPDATE Thermostats Set  Fan_Status = %s, Compressor_Status = %s, Compressor_Time = %s, Fan_Time = %s WHERE Id = %s", (Fan_Status, Compressor_Status, Compressor_Time, Fan_Time, loop))
 
 #sleep to make sure compressor is not killed on statup
-time.sleep(180)
+#time.sleep(10)
 
 
 while True:
@@ -49,70 +51,64 @@ while True:
 		cursor.execute("SELECT * FROM Thermostats WHERE Id = %s", loop)
 		rows = cursor.fetchall()
 		for row in rows:
-			Sensor_Pin = int(row["Sensor_Pin"])
 			Fan_Pin = int(row["Fan_Pin"])
 			Compressor_Pin = int(row["Compressor_Pin"])
 			Temp_Target = int(row["Temp_Target"])
-			Humidity_Target = int(row["Humidity_Target"])
 			Fan_Status = bool(row["Fan_Status"])
 			Compressor_Status = bool(row["Compressor_Status"])
 			Enabled = bool(row["Enabled"])
-			Enabled_Time = (row["Enabled_Time"])
 			Fan_Time = (row["Fan_Time"])
 			Compressor_Time = (row["Compressor_Time"])
 			Fan_Change_Recent = bool(row["Fan_Change_Recent"])
 			Compressor_Change_Recent = bool(row["Compressor_Change_Recent"])
-		#calculate timedeltas	
-		current_time = datetime.now()
-		fan_time_delta = timedelta(current_time - Fan_Time)
-		compressor_time_delta = timedelta(current_time - Compressor_Time)
-		enabled_time_delta = timedelta(current_time - Enabled_Time)
-
-		#get actual temperature and humidity
-		humidity, temperature = Adafruit_DHT.read_retry(sensor, Sensor_Pin)
-		Temp_Actual = int((temperature * 1.8) + 32)
-		Humidity_Actual = int(humidity)	
+		
+		#get actual temperature
+		bus = smbus.SMBus(1)
+		tmp102 = bus.read_i2c_block_data(0x48, 0)
+		tmp1020 = tmp102[0]
+		tmp1021 = tmp102[1]
+		Temp_Actual = int((((((tmp1020 << 8) | tmp1021) >> 4) * 0.0625) *1.8 ) + 32 )
+		
 
 		#put actual temp and humidity in DB
-		cursor.execute("UPDATE Thermostats Set Temp_Actual = %s, Humidity_Actual = %s WHERE Id = %s", (Temp_Actual, Humidity_Actual, loop))	
+		cursor.execute("UPDATE Thermostats SET Temp_Actual = %s WHERE Id = %s", (Temp_Actual, loop))	
 
-		#if loop to check if the compressor has been turned on or off recently
-		if (compressor_time_delta.total_seconds() >	180): 
-			#if loop to check for enabled
-			if(Enabled == 1):
-				#if loop to check actual vs target temps and turn on/off AC
-				if (Temp_Target < Temp_Actual):
-					print "Turn on AC"
-					Fan_Status = True
-					Compressor_Status = True
-					cursor.execute("UPDATE Thermostats Set Fan_Status = %s, Compressor_Status = %s  WHERE Id = %s", (Fan_Status, Compressor_Status, loop))
-					GPIO.output(Fan_Pin, 0)
-					GPIO.output(Compressor_Pin, 0)
-				else:
-					print "Turn off AC"
-					Fan_Status = False
-					Compressor_Status = False
-					Compressor_Time = now()
-					cursor.execute("UPDATE Thermostats Set Fan_Status = %s, Compressor_Status = %s, Compressor_Time = %s  WHERE Id = %s", (Fan_Status, Compressor_Status, Compressor_Time, loop))
-					GPIO.output(Fan_Pin, 1)
-					GPIO.output(Compressor_Pin, 1)	
+		#Loop to adjust compressor and fan time
+		if (Fan_Time != 0):
+			Fan_Time = (Fan_Time - 1)
+			cursor.execute("UPDATE Thermostats Set Fan_Time = %s  WHERE Id = %s", (Fan_Time, loop))
+		if (Compressor_Time != 0):
+			Compressor_Time = (Compressor_Time - 1)
+			cursor.execute("UPDATE Thermostats Set Compressor_Time = %s  WHERE Id = %s", (Compressor_Time, loop))
 
-				if (Humidity_Target < Humidity_Actual):
-					print "Turn on AC"
-					Fan_Status = True
-					Compressor_Status = True
-					cursor.execute("UPDATE Thermostats Set Fan_Status = %s, Compressor_Status = %s  WHERE Id = %s", (Fan_Status, Compressor_Status, loop))
-					GPIO.output(Fan_Pin, 0)
-					GPIO.output(Compressor_Pin, 0)
-				else:
-					print "Turn off AC"
-					Fan_Status = False
-					Compressor_Status = False
-					cursor.execute("UPDATE Thermostats Set Fan_Status = %s, Compressor_Status = %s  WHERE Id = %s", (Fan_Status, Compressor_Status, loop))
-					GPIO.output(Fan_Pin, 1)
-					GPIO.output(Compressor_Pin, 1)
+		
+		if(Enabled == 1 and Compressor_Time == 0):
+			#if loop to check actual vs target temps and turn on/off AC
+			if (Temp_Target < Temp_Actual and Compressor_Status != True):
+				print "Turn on AC"
+				Fan_Status = True
+				Compressor_Status = True
+				Compressor_Time = 300
+				cursor.execute("UPDATE Thermostats Set Fan_Status = %s, Compressor_Status = %s, Compressor_Time = %s  WHERE Id = %s", (Fan_Status, Compressor_Status, Compressor_Time, loop))
+				GPIO.output(Fan_Pin, 0)
+				GPIO.output(Compressor_Pin, 0)
+			if (Temp_Target > Temp_Actual and Compressor_Status == True):
+				print "Turn off AC"
+				Compressor_Status = False
+				Compressor_Time = 300
+				Fan_Time = 300
+				cursor.execute("UPDATE Thermostats Set Compressor_Status = %s, Compressor_Time = %s, Fan_Time = %s  WHERE Id = %s", (Compressor_Status, Compressor_Time, Fan_Time, loop))
+				GPIO.output(Compressor_Pin, 1)	
+
+		#Run fan to continue cooling for 5 min
+		if (Compressor_Time == 1):
+			Fan_Status = False
+			cursor.execute("UPDATE Thermostats Set Fan_Status = %s WHERE Id = %s", (Fan_Status, loop))
+			GPIO.output(Fan_Pin, 1)
+
 		db.commit()
 
+	time.sleep(1)
 
  
 db.close()
